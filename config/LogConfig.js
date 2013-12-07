6f5298f4e87f71449c57e485cbac82aa9fa8df6f
Christoph Hartmann <chris@lollyrock.com>
2013-12-07 19:16:26 +0100

    improvement: make log levels configurable

--- config/LogConfig.js
+++ config/LogConfig.js
@@ -2,74 +2,81 @@
 
 var winston = require('winston');
 
-winston.loggers.add('xrocket', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'xrocket'
-    }
-});
+var LogConfig = function () {};
 
-winston.loggers.add('cm', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'cm'
-    }
-});
+LogConfig.prototype.configure = function (level) {
 
-winston.loggers.add('router', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'router'
-    }
-});
+    winston.loggers.add('xrocket', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'xrocket'
+        }
+    });
 
-winston.loggers.add('connrouter', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'connrouter'
-    }
-});
+    winston.loggers.add('cm', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'cm'
+        }
+    });
 
-winston.loggers.add('logrouter', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'logrouter'
-    }
-});
+    winston.loggers.add('router', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'router'
+        }
+    });
 
-winston.loggers.add('xeprouter', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'xeprouter'
-    }
-});
+    winston.loggers.add('connrouter', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'connrouter'
+        }
+    });
 
-winston.loggers.add('xepcomponent', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'component'
-    }
-});
+    winston.loggers.add('logrouter', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'logrouter'
+        }
+    });
 
-winston.loggers.add('authentication', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'auth'
-    }
-});
+    winston.loggers.add('xeprouter', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'xeprouter'
+        }
+    });
 
-winston.loggers.add('postgresql', {
-    console: {
-        level: 'silly',
-        colorize: 'true',
-        label: 'postgresql'
-    }
-});
+    winston.loggers.add('xepcomponent', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'component'
+        }
+    });
+
+    winston.loggers.add('authentication', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'auth'
+        }
+    });
+
+    winston.loggers.add('postgresql', {
+        console: {
+            level: level,
+            colorize: 'true',
+            label: 'postgresql'
+        }
+    });
+};
+
+module.exports = new LogConfig();
