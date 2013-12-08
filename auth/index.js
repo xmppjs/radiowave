ef89b75ebfccd691f2fac495ea90c3c8a46f5d15
Christoph Hartmann <chris@lollyrock.com>
2013-12-08 18:05:41 +0100

    feature: export module functionality

---
+++ auth/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    Simple: require('./Simple')
+};
---
+++ xep/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    Rfc3921Messaging: require('./Rfc3921-messaging'),
+    Rfc3921Roaster : require('./Rfc3921-roaster'),
+    Xep0030 : require('./Xep0030-disco'),
+    Xep0092 : require('./Xep0092-version'),
+    Xep0199 : require('./Xep0199-ping')
+};
