ee20a0f7b1709ee4c318acbebba694f69e0498bd
Christoph Hartmann <chris@lollyrock.com>
2013-12-08 18:05:41 +0100

    feature: export module functionality

---
+++ router/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    BaseRouter : require('./BaseRouter'),
+    ComponentRouter : require('./ComponentRouter'),
+    ConnectionRouter : require('./ConnectionRouter'),
+    LogRouter : require('./LogRouter')
+};
---
+++ xrocket.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    Xep : require('./xep'),
+    Router : require('./router'),
+    Net : require('./net'),
+    Auth :require('./auth'),
+    XRocket : require('./core/XRocket')
+};
