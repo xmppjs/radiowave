0212ff9f691d9685141d67d86791a3510adec612
Christoph Hartmann <chris@lollyrock.com>
2013-12-30 18:49:53 +0100

    add muc unique implementation

---
+++ xep/Xep0307-mucunique.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var ltx = require('ltx'),
+    util = require('util'),
+    winston = require('winston'),
+    logger = winston.loggers.get('xepcomponent'),
+    uuid = require('node-uuid'),
+    XepComponent = require('./XepComponent');
+
+var NS_MUC_UNIQUE = 'http://jabber.org/protocol/muc#unique';
+
+/*
+ * XEP-0307: Unique Room Names for Multi-User Chat
+ * http://xmpp.org/extensions/xep-0307.html
+ *
+ */
+function MucUnique() {
+    XepComponent.call(this);
+}
+util.inherits(MucUnique, XepComponent);
+
+MucUnique.prototype.name = 'XEP-0307: Unique Room Names for Multi-User Chat';
+
+MucUnique.prototype.version = '0.1.0';
+
+MucUnique.prototype.match = function (stanza) {
+    logger.debug("check match");
+    if (stanza.is('iq') && stanza.attrs.type === 'get' && (stanza.getChild('unique', NS_MUC_UNIQUE))) {
+        logger.debug('detected muc unique request');
+        return true;
+    }
+    return false;
+};
+
+/**
+ * @description Entity Requests Unique Room Name
+ * @param stanza full pubsub message stanza
+ * @see http://xmpp.org/extensions/xep-0307.html
+ * 
+ * <iq from='crone1@shakespeare.lit/desktop'
+ *     id='unique1'
+ *     to='chat.shakespeare.lit'
+ *     type='get'>
+ *   <unique xmlns='http://jabber.org/protocol/muc#unique'/>
+ * </iq>
+ *
+ * <iq from='chat.shakespeare.lit'
+ *     id='unique1'
+ *     to='crone1@shakespeare.lit/desktop'
+ *     type='result'>
+ *   <unique xmlns='http://jabber.org/protocol/muc#unique'>
+ *     6d9423a55f499b29ad20bf7b2bdea4f4b885ead1
+ *   </unique>
+ * </iq>
+ */
+MucUnique.prototype.handle = function (stanza) {
+    // stanza is already verified
+
+    var msg = new ltx.Element('iq', {
+        from: stanza.attrs.to,
+        to: stanza.attrs.from,
+        id: stanza.attrs.id,
+        type: 'result'
+    });
+    var id = uuid.v4().toString();
+
+    // TODO verify the id does not exist in our database
+    msg.c('unique', {xmlns : NS_MUC_UNIQUE }).t(id);
+
+    this.send(msg);
+};
+
+module.exports = MucUnique;
