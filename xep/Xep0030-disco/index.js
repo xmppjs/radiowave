675018826c3507333c51dabb03bed3fbf153c3c6
Christoph Hartmann <chris@lollyrock.com>
2014-01-11 19:02:25 +0100

    move disco implementation into subfolder

--- xep/Xep0030-disco/index.js
+++ xep/Xep0030-disco/index.js
@@ -4,7 +4,7 @@ var ltx = require('ltx'),
     util = require('util'),
     winston = require('winston'),
     logger = winston.loggers.get('xepcomponent'),
-    XepComponent = require('./XepComponent');
+    XepComponent = require('../XepComponent');
 
 var NS_DISCO_INFO = 'http://jabber.org/protocol/disco#info';
 //  NS_DISCO_ITEM = 'http://jabber.org/protocol/disco#items';
