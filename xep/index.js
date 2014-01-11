364a3cbe6162bfb59c13bc84c7a4ad284081284c
Christoph Hartmann <chris@lollyrock.com>
2014-01-11 21:12:03 +0100

    export xep components

--- xep/index.js
+++ xep/index.js
@@ -5,6 +5,10 @@ module.exports = {
     Rfc3921Messaging: require('./Rfc3921-messaging'),
     Rfc3921Roaster : require('./Rfc3921-roaster'),
     Xep0030 : require('./Xep0030-disco'),
+    Xep0045 : require('./Xep0045-muc'),
+    Xep0054 : require('./Xep0054-vcard'),
+    Xep0060 : require('./Xep0060-pubsub'),
     Xep0092 : require('./Xep0092-version'),
-    Xep0199 : require('./Xep0199-ping')
+    Xep0199 : require('./Xep0199-ping'),
+    Xep0307 : require('./Xep0307-mucunique')
 };
