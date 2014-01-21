6810873e1807f117b3696765205689977e871441
Christoph Hartmann <chris@lollyrock.com>
2014-01-21 22:39:49 +0100

    improvement: enable external http server for BOSH

--- net/BOSHServer.js
+++ net/BOSHServer.js
@@ -3,7 +3,7 @@
 var http = require('http'),
     util = require('util'),
     EventEmitter = require('events').EventEmitter,
-    XmppBOSHServer = require('./bosh/BOSHServer'),//require('node-xmpp-server').BOSHServer,
+    XmppBOSHServer = require('./bosh/BOSHServer'), //require('node-xmpp-server').BOSHServer,
     C2SStream = require('node-xmpp-server').C2SStream;
 
 var winston = require('winston'),
@@ -11,13 +11,20 @@ var winston = require('winston'),
 
 /**
  * [BOSHServer description]
- *   options : BOSH_PORT
- *   options.port : xmpp tcp socket port
+ *
+ *   options.port : BOSH_PORT
+ *   options.autostart : autostarts the server
+ *
+ * Some default ports are:
+ * - http://example.com:5280/http-bind
+ * - https://example.com:5281/http-bind
  */
 function BOSHServer(options) {
     this.options = options ||  {};
 
-    this.port = this.options.port || this.BOSH_PORT;
+    if (!options.autostart) {
+        options.autostart = true;
+    }
 
     EventEmitter.call(this);
 
@@ -25,11 +32,6 @@ function BOSHServer(options) {
     this.c2s = null;
 
     this.bosh = new XmppBOSHServer();
-
-    // start bosh http port 
-    http.createServer(function (req, res) {
-        self.bosh.handleHTTP(req, res);
-    }).listen(options.port);
 
     // listen for connect events from bosh
     this.bosh.on('connect', function (boshsession) {
@@ -47,6 +49,16 @@ function BOSHServer(options) {
     this.bosh.on('error', function (error) {
         logger.error(error);
     });
+
+    // start bosh server
+    if (this.options.autostart) {
+        this.port = this.options.port || this.BOSH_PORT;
+
+        // start bosh http port 
+        http.createServer(function (req, res) {
+            self.bosh.handleHTTP(req, res);
+        }).listen(options.port);
+    }
 }
 
 util.inherits(BOSHServer, EventEmitter);
