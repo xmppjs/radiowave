58808fcdde54b21f0ebafc3548915ceb806406a4
Christoph Hartmann <chris@lollyrock.com>
2014-01-23 17:20:22 +0100

    enable xep components to emit events

--- xep/XepComponent.js
+++ xep/XepComponent.js
@@ -1,71 +1,78 @@
 'use strict';
 
-var ltx = require('ltx');
+var ltx = require('ltx'),
+    util = require('util'),
+    EventEmitter = require('events').EventEmitter;
 
 function XepComponent() {}
 
-XepComponent.prototype = {
-    name: 'XepComponent',
-    version: '0.0.0',
-    /**
-     * This method will initialize the module. It is called by component router before the server
-     * is finished with startup.
-     */
-    initialize: function () {},
-    /**
-     * send messages and start routing
-     */
-    send: function () {},
-    /**
-     * verify if a given stanza can be handled by this module
-     */
-    match: function () {},
-    /**
-     * handles xmpp stanzas
-     */
-    handle: function () {},
+util.inherits(XepComponent, EventEmitter);
 
-    /**
-     * Send an error to the original sender of the message
-     *
-     * @params stanza original message from the sender
-     */
-    sendError: function (stanza, err) {
-        var response = new ltx.Element(stanza.getName(), {
-            from: stanza.attrs.to,
-            to: stanza.attrs.from,
-            id: stanza.attrs.id,
-            type: 'error'
-        });
+XepComponent.prototype.name = 'XepComponent';
 
-        // attach error detail
-        if (err) {
-            response.cnode(err);
-        }
+XepComponent.prototype.version = '0.0.0';
 
-        this.send(response);
-    },
+/**
+ * This method will initialize the module. It is called by component router before the server
+ * is finished with startup.
+ */
+XepComponent.prototype.initialize = function () {};
 
-    /**
-     * Respond with success to the original message
-     *
-     * @params stanza original message from the sender
-     */
-    sendSuccess: function (stanza, detail) {
-        var response = new ltx.Element('iq', {
-            from: stanza.attrs.to,
-            to: stanza.attrs.from,
-            id: stanza.attrs.id,
-            type: 'result'
-        });
+/**
+ * send messages and start routing
+ */
+XepComponent.prototype.send = function () {};
 
-        // attach error detail
-        if (detail) {
-            response.cnode(detail);
-        }
+/**
+ * verify if a given stanza can be handled by this module
+ */
+XepComponent.prototype.match = function () {};
 
-        this.send(response);
+/**
+ * handles xmpp stanzas
+ */
+XepComponent.prototype.handle = function () {};
+
+/**
+ * Send an error to the original sender of the message
+ *
+ * @params stanza original message from the sender
+ */
+XepComponent.prototype.sendError = function (stanza, err) {
+    var response = new ltx.Element(stanza.getName(), {
+        from: stanza.attrs.to,
+        to: stanza.attrs.from,
+        id: stanza.attrs.id,
+        type: 'error'
+    });
+
+    // attach error detail
+    if (err) {
+        response.cnode(err);
     }
+
+    this.send(response);
+};
+
+/**
+ * Respond with success to the original message
+ *
+ * @params stanza original message from the sender
+ */
+XepComponent.prototype.sendSuccess = function (stanza, detail) {
+    var response = new ltx.Element('iq', {
+        from: stanza.attrs.to,
+        to: stanza.attrs.from,
+        id: stanza.attrs.id,
+        type: 'result'
+    });
+
+    // attach error detail
+    if (detail) {
+        response.cnode(detail);
+    }
+
+    this.send(response);
 };
 
 module.exports = XepComponent;
