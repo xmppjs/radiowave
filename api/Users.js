21aa6b98a8ce8ff3a96d2377c6581ddf1217f806
Christoph Hartmann <chris@lollyrock.com>
2014-01-26 15:12:52 +0100

    api: properly check input data and implement portions of channel api

--- api/Users.js
+++ api/Users.js
@@ -185,13 +185,15 @@ var routes = function (app, storage) {
      * {
      *    "name" : "room_name"
      * }
-     *
      */
     app.post('/api/user/rooms', function (req, res) {
         var username = getUsername(req);
         var data = req.body;
 
-        // TODO check that all required parameters are properly set
+        // check that all required parameters are properly set
+        if (!data || !data.name || data.name === '' || data.name === null) {
+            res.json(new ApiError('name is missing'));
+        }
 
         Users.user(username).then(
             function (user) {
@@ -199,7 +201,68 @@ var routes = function (app, storage) {
             }).then(
             function (room) {
                 res.json(room);
+            }).catch(
+            function (error) {
+                res.json(new ApiError(error));
+            });
+    });
+
+    /**
+     * List channels for the authenticated user.
+     */
+    app.get('/api/user/channels', function (req, res) {
+        var username = getUsername(req);
+        var usr = null;
+
+        Users.user(username).then(
+            function (user) {
+                usr = user;
+                return user.listChannels();
+            }).then(
+            function (channels) {
+                console.log(JSON.stringify(channels));
+                
+                // iterate over rooms and generate json
+                var channelPromisses = channels.map(function (roomname) {
+                    console.log(roomname);
+                    console.log(usr);
+                    return usr.getChannel(roomname);
+                });
+
+                console.log('wait for response');
+                Promise.all(channelPromisses).then(function (contentChannel) {
+                    console.log('raw' + JSON.stringify(contentChannel));
+
+                    var data = [];
+                    contentChannel.forEach(function (ch) {
+                        data.push(ch.toJSON());
+                    });
+                    res.json(data);
+                }).catch (function (reason) {
+                    res.json(new ApiError(reason));
+                });
             },
+            function (error) {
+                res.json(new ApiError(error));
+            });
+    });
+
+    app.post('/api/user/channels', function (req, res) {
+        var username = getUsername(req);
+        var data = req.body;
+
+        // check that all required parameters are properly set
+        if (!data || !data.name || data.name === '' || data.name === null) {
+            res.json(new ApiError('name is missing'));
+        }
+
+        Users.user(username).then(
+            function (user) {
+                return user.createChannel(data.name);
+            }).then(
+            function (room) {
+                res.json(room);
+            }).catch(
             function (error) {
                 res.json(new ApiError(error));
             });
