568ad8b5f90a8185433d4db4dcf4df9febbf0a68
Christoph Hartmann <chris@lollyrock.com>
2014-01-31 18:00:51 +0100

    moved app and api into a separate repo and separated core xmpp code

--- doc/api/channels/events.md
+++
@@ -1,9 +0,0 @@
-# Event
-
-## trigger an event
-
-    POST /channels/:owner/:channel/messages
-
-## List events for a channel
-
-    GET /channels/:owner/:channel/messages
--- xrocket.js
+++ xrocket.js
@@ -1,12 +1,6 @@
 'use strict';
 
-var winston = require('winston');
-
-// set log level
-require('./config/LogConfig')('info');
-
 module.exports = {
-    Api : require('./api/routes'),
     Xep : require('./xep'),
     Router : require('./router'),
     Net : require('./net'),
