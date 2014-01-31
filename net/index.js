5e26afc8895f35dfc09204c0ea4d3ea7485bbedd
Christoph Hartmann <chris@lollyrock.com>
2014-01-31 18:08:51 +0100

    move connection manager into separate repo

--- net/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    C2SServer : require('./C2SServer'),
-    BOSHServer : require('./BOSHServer'),
-    WsServer : require('./WsServer'),
-    SioServer : require('./SioServer')
-};
--- xrocket.js
+++ xrocket.js
@@ -3,7 +3,6 @@
 module.exports = {
     Xep : require('./xep'),
     Router : require('./router'),
-    Net : require('./net'),
     Auth :require('./auth'),
     XRocket : require('./core/XRocket'),
     Storage : require('./storage/in-memory')
