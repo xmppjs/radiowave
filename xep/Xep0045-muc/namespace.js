7d2d03e6e072358d148316f24a82fe346b16400a
Christoph Hartmann <chris@lollyrock.com>
2014-02-01 17:30:23 +0100

    add more namespaces

--- xep/Xep0045-muc/namespace.js
+++ xep/Xep0045-muc/namespace.js
@@ -1,9 +1,11 @@
 // pubsub namespaces
 module.exports = {
-    'DISCO_ITEMS' : 'http://jabber.org/protocol/disco#items',
-    'DISCO_INFO' : 'http://jabber.org/protocol/disco#info',
-    'MUC_USER' : 'http://jabber.org/protocol/muc#user',
-    'MUC' : 'http://jabber.org/protocol/muc',
-    'MUC_UNIQUE' : 'http://jabber.org/protocol/muc#unique'
+    'DISCO_ITEMS'   : 'http://jabber.org/protocol/disco#items',
+    'DISCO_INFO'    : 'http://jabber.org/protocol/disco#info',
+    'MUC'           : 'http://jabber.org/protocol/muc',
+    'MUC_USER'      : 'http://jabber.org/protocol/muc#user',
+    'MUC_ADMIN'     : 'http://jabber.org/protocol/muc#admin',
+    'MUC_OWNER'     : 'http://jabber.org/protocol/muc#owner',
+    'MUC_UNIQUE'    : 'http://jabber.org/protocol/muc#unique'
 };
 
