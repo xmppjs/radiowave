91013c784448fc2a4f0c8502256c49fa689d8c25
Christoph Hartmann <chris@lollyrock.com>
2014-02-06 08:59:18 +0100

    bugfix: add missing authenticator

---
+++ auth/Authenticator.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Promise = require('bluebird');
+
+function Authenticator() {}
+
+/**
+ * @param method name of the method
+ */
+Authenticator.prototype.match = function (method){
+    return false;
+};
+
+/**
+ * @param opts options hash-key map 
+ */
+Authenticator.prototype.authenticate = function (opts){
+    return new Promise(function (resolve, reject) {
+        reject();
+    });
+};
+
+module.exports = Authenticator;
