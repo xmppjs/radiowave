21e3a24dacc4f3520ad679f5d514805190cba6ef
Christoph Hartmann <chris@lollyrock.com>
2014-02-08 12:36:53 +0100

    verify jid username with oauth token result

--- auth/Oauth2.js
+++ auth/Oauth2.js
@@ -4,6 +4,8 @@ var util = require('util'),
     Authenticator = require('./Authenticator'),
     winston = require('winston'),
     Promise = require('bluebird'),
+    JID = require('node-xmpp-core').JID,
+    Escape = require('node-xmpp-core').Escape,
     superagent = require('superagent'),
     logger = winston.loggers.get('authentication');
 
@@ -12,6 +14,11 @@ var util = require('util'),
  */
 function OAUTH2(settings) {
     this.settings = settings;
+    this.url = settings.url;
+    this.contentType = settings.contentType || 'application/json';
+    this.tokenType = settings.tokenType || 'Bearer';
+    this.uidTag = settings.uidTag || 'login';
+
 }
 util.inherits(OAUTH2, Authenticator);
 
@@ -24,40 +31,79 @@ OAUTH2.prototype.match = function (method) {
     return false;
 };
 
-OAUTH2.prototype.verifyToken = function (oauthToken, cb) {
+OAUTH2.prototype.verifyToken = function (username, oauthToken, cb) {
     var self = this;
 
     logger.debug('oauth2 call: ' + this.settings.url);
 
     // load user details
     superagent
-        .post(this.settings.url)
+        .post(self.url)
         .send({})
-        .set('content-type', 'application/json')
-        .set('Authorization', 'Bearer ' + oauthToken)
+        .set('content-type', self.contentType)
+        .set('Authorization', self.tokenType + ' ' + oauthToken)
         .end(function (error, res) {
             if (error ||  res.status !== 200) {
                 logger.error(error);
                 console.log(error);
                 cb('oauth authentication failed');
             } else {
-                cb(null, self.extractUser(res.body));
+                // we know the token is valid
+                // verify it against a username
+                var usr = self.verifyUser(username , res.body);
+
+                cb(null, usr);
             }
         });
 };
 
-OAUTH2.prototype.extractUser = function(content) {
-    return content;
+/** 
+ * @param username username that was submitted
+ * @param user user object we got from token verification
+ * @returns null, if user does not match username, otherwise the userobject
+ */
+OAUTH2.prototype.verifyUser = function (username, content) {
+    logger.debug('verify user: ' + username + ' -> ' + content[this.uidTag]);
+
+    // for api request we have only a token, but thats enough
+    if (!username) {
+        return content;
+    }
+    // if we use xmpp, we verify the jid against the username
+    else if (content[this.uidTag] === username) {
+        return content;
+    } else {
+        return null;
+    }
 };
 
 OAUTH2.prototype.authenticate = function (opts) {
+
+    var username = null;
+
+    // generate ldap username 
+    if (opts.jid) {
+        username = Escape.unescapeLocal(new JID(opts.jid.toString()).getLocal());
+    } else if (opts.username) {
+        username = opts.username;
+    }
+
+    opts.username = username;
+
     var self = this;
     return new Promise(function (resolve, reject) {
         logger.info("OAUTH2 authenticate ", opts.oauth_token);
-        self.verifyToken(opts.oauth_token, function (err, user){
+        self.verifyToken(opts.username, opts.oauth_token, function (err, user){
+            // error
             if (err) {
                 reject('OAUTH2 could not authenticate user: ' + opts.username);
-            } else {
+            }
+            // token does not match username
+            else if (user === null) {
+                reject('OAUTH2 could not authenticate user: ' + opts.username);
+            }
+            // everything is okay
+            else {
                 logger.debug('OAUTH2: token ' + opts.oauth_token + ' is valid ' + JSON.stringify(user));
                 resolve(user);
             }
