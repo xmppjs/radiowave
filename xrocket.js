d186ff73fd7d18149e6f682f6e968632b2780cc3
Christoph Hartmann <chris@lollyrock.com>
2014-02-22 15:25:52 +0100

    export modules and rename module

--- xrocket.js
+++ xrocket.js
@@ -4,6 +4,8 @@ module.exports = {
     Xep : require('./xep'),
     Router : require('./router'),
     Auth :require('./auth'),
+    Net : require('./net'),
     XRocket : require('./core/XRocket'),
-    Storage : require('./storage/in-memory')
+    Storage : require('./storage/in-memory'),
+    Api : require('./api/routes')
 };
