78169fe5b35de481dec7821178e42005571b611b
Christoph Hartmann <chris@lollyrock.com>
2014-03-05 16:50:39 +0100

    add muc tests

---
+++ test/_helper/helper.js
@@ -0,0 +1,152 @@
+'use strict';
+
+var winston = require('winston');
+
+var Promise = require('bluebird'),
+    Client = require('node-xmpp-client'),
+    xRocket = require('../../xrocket'),
+    C2SServer = xRocket.Net.C2SServer;
+
+// user
+var userRomeo = {
+    jid: 'romeo@example.net',
+    password: 'romeo',
+    host: 'localhost'
+};
+
+var userJulia = {
+    jid: 'julia@example.net',
+    password: 'julia',
+    host: 'localhost'
+};
+
+var userBenvolio = {
+    jid: 'benvolio@example.net',
+    password: 'benvolio',
+    host: 'localhost'
+};
+
+// start client and wait until the client is online
+function startClient(user) {
+    var promise = new Promise(function (resolve, reject) {
+        var cl = new Client({
+            jid: user.jid,
+            password: user.password,
+            preferred: 'PLAIN',
+            host: user.host
+        });
+        cl.on('online', function () {
+            resolve(cl);
+        });
+        cl.on('error', function () {
+            reject();
+        });
+    });
+    return promise;
+}
+
+function startRomeo() {
+    return startClient(userRomeo);
+}
+
+function startJulia() {
+    return startClient(userJulia);
+}
+
+function startBenvolio() {
+    return startClient(userBenvolio);
+}
+
+function startServer() {
+    var promise = new Promise(function (resolve, reject) {
+
+        // inistanciate storage module
+        // use sqlite for testing
+        var s = new xRocket.Storage({
+            database: 'database',
+            username: 'username',
+            password: 'password',
+            dialect: 'sqlite',
+            storage: './test.sqlite'
+        });
+        s.initialize().then(function(storage){
+
+            // C2S Server 
+            var cs2 = new C2SServer({});
+
+            // attach connection manager to xrocket
+            var xR = new xRocket.XRocket();
+
+            xR.addConnectionRouter(new xRocket.Router.ConnectionRouter(storage));
+            xR.addConnectionManager(cs2);
+
+            // register users
+            var simpleAuth = new xRocket.Auth.Simple();
+            simpleAuth.addUser('romeo', 'romeo');
+            simpleAuth.addUser('julia', 'julia');
+            simpleAuth.addUser('benvolio', 'benvolio');
+            xR.connectionRouter.authMethods.push(simpleAuth);
+
+            // register xep component
+            var cr = new xRocket.Router.ComponentRouter({
+                domain: 'example.net'
+            });
+            var lr = new xRocket.Router.LogRouter();
+            // chain XRocket to ComponentRouter
+            xR.chain(lr).chain(cr);
+
+            var returnVal = {
+                'xR': xR,
+                'cr': cr,
+                'storage': storage
+            };
+            resolve(returnVal);
+        }).catch(function(err){
+            reject(err);
+        });
+    });
+    return promise;
+}
+
+function configureLoglevel(level) {
+    console.log('configure xrocket logging');
+
+    var defaultConfiguration = {
+        console: {
+            level: level,
+            colorize: 'true'
+        }
+    };
+
+    function getConfiguration (label) {
+        var conf = Object.create(defaultConfiguration);
+        conf.console.label = label;
+        return conf;
+    }
+    
+    winston.loggers.add('xrocket', getConfiguration('xrocket'));
+    winston.loggers.add('cm', getConfiguration('cm'));
+    winston.loggers.add('router', getConfiguration('router'));
+    winston.loggers.add('connrouter', getConfiguration('connrouter'));
+    winston.loggers.add('logrouter', getConfiguration('logrouter'));
+    winston.loggers.add('xeprouter', getConfiguration('xeprouter'));
+    winston.loggers.add('xepcomponent', getConfiguration('xepcomponent'));
+    winston.loggers.add('xep-0045', getConfiguration('xep-0045'));
+    winston.loggers.add('xep-0060', getConfiguration('xep-0060'));
+    winston.loggers.add('authentication', getConfiguration('authentication'));
+    winston.loggers.add('postgresql', getConfiguration('postgresql'));
+    winston.loggers.add('storage', getConfiguration('storage'));
+    winston.loggers.add('webapi', getConfiguration('webapi'));
+
+    winston.loggers.add('websocket', getConfiguration('websocket'));
+    winston.loggers.add('socketio', getConfiguration('socketio'));
+    winston.loggers.add('bosh', getConfiguration('bosh'));
+}
+
+module.exports = {
+    'configureLoglevel' : configureLoglevel,
+    'startRomeo': startRomeo,
+    'startJulia': startJulia,
+    'startBenvolio': startBenvolio,
+    'startServer': startServer,
+};
---
+++ test/xep0045-muc/xep0045-muc-admin.js
@@ -0,0 +1,358 @@
+'use strict';
+
+var Promise = require('bluebird'),
+    ltx = require('ltx'),
+    assert = require('assert'),
+    should = require('should'),
+    helper = require('../_helper/helper'),
+    muc_helper = require('../_helper/muc');
+
+// logging
+helper.configureLoglevel('silly');
+
+var Xep0045 = require('../../xep/Xep0045-muc');
+
+function configureXEP(server) {
+    // register pubsub component
+    server.cr.register(new Xep0045({
+        subdomain: 'chat',
+        domain: 'example.net',
+        storage: server.storage
+    }));
+}
+    
+/**
+ * @see http://xmpp.org/extensions/xep-0045.html#admin
+ */
+describe('Xep-0060', function () {
+    describe('9. Admin Use Cases', function () {
+
+        var room = 'julia_room_01@chat.example.net';
+        var nick = 'julia';
+        var srv = null;
+
+        before(function (done) {
+            helper.startServer()
+            // configure muc module
+            .then(function (server) {
+                srv = server;
+                configureXEP(server);
+                done();
+            })
+                .
+            catch (function (err) {
+                done(err);
+            });
+        });
+
+        after(function (done) {
+            srv.xR.shutdown();
+            done();
+        });
+
+        it('Precondition: create room', function (done) {
+            muc_helper.juliaCreateRoom(room, nick, done);
+        });
+
+        /*
+         * Example 110. Admin Bans User
+         * <iq from='kinghenryv@shakespeare.lit/throne'
+         *     id='ban1'
+         *     to='southampton@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='outcast'
+         *           jid='earlofcambridge@shakespeare.lit'/>
+         *   </query>
+         * </iq>
+         *
+         * Service Informs Admin or Owner of Success
+         * <iq from='southampton@chat.shakespeare.lit'
+         *     id='ban1'
+         *     to='kinghenryv@shakespeare.lit/throne'
+         *     type='result'/>
+         *
+         * Service Removes Banned User
+         * <presence
+         *     from='southampton@chat.shakespeare.lit/cambridge'
+         *     to='earlofcambridge@shakespeare.lit/stabber'
+         *     type='unavailable'>
+         *   <x xmlns='http://jabber.org/protocol/muc#user'>
+         *     <item affiliation='outcast' role='none'>
+         *       <actor nick='The ♚'/>
+         *       <reason>Treason</reason>
+         *     </item>
+         *     <status code='301'/>
+         *   </x>
+         * </presence>
+         *
+         * Service Informs Remaining Occupants
+         * <presence
+         *     type='unavailable'
+         *     from='southampton@chat.shakespeare.lit/cambridge'
+         *     to='exeter@shakespeare.lit/pda'>
+         *   <x xmlns='http://jabber.org/protocol/muc#user'>
+         *     <item affiliation='outcast'
+         *           jid='earlofcambridge@shakespeare.lit/stabber'
+         *           role='none'/>
+         *     <status code='301'/>
+         *   </x>
+         * </presence>
+         */
+        it('9.1 Banning a User', function (done) {
+            done();
+        });
+
+        /*
+         *
+         * Admin Bans User (With a Reason)
+         * <iq from='kinghenryv@shakespeare.lit/throne'
+         *     id='ban1'
+         *     to='southampton@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='outcast'
+         *           jid='earlofcambridge@shakespeare.lit'>
+         *       <reason>Treason</reason>
+         *     </item>
+         *   </query>
+         * </iq>
+         */
+        it('9.1 Banning a User', function (done) {
+            done();
+        });
+
+        /*
+         * Service Returns Error on Attempt to Ban User With Higher Affiliation
+         * <iq from='kinghenryv@shakespeare.lit/throne'
+         *     id='ban1'
+         *     to='southampton@chat.shakespeare.lit'
+         *     type='set'>
+         *   <error type='cancel'>
+         *     <not-allowed xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/>
+         *   </error>
+         * </iq>
+         */
+        it('9.1 Banning a User', function (done) {
+            done();
+        });
+
+        /*
+         *
+         * <iq from='kinghenryv@shakespeare.lit/throne'
+         *     id='ban2'
+         *     to='southampton@chat.shakespeare.lit'
+         *     type='get'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='outcast'/>
+         *   </query>
+         * </iq>
+         *
+         * <iq from='southampton@chat.shakespeare.lit'
+         *     id='ban2'
+         *     to='kinghenryv@shakespeare.lit/throne'
+         *     type='result'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='outcast'
+         *           jid='earlofcambridge@shakespeare.lit'>
+         *       <reason>Treason</reason>
+         *     </item>
+         *   </query>
+         * </iq>
+         */
+        it('9.2 Admin Requests Ban List', function (done) {
+            var julia;
+
+            var nickjid = room + '/' + nick;
+
+            var msg = 
+            "<iq id='ban2' type='get'> \
+                <query xmlns='http://jabber.org/protocol/muc#admin'> \
+                  <item affiliation='outcast'/> \
+                </query> \
+            </iq>";
+
+            // start clients
+            Promise.all([helper.startJulia()]).then(function (results) {
+                    julia = results[0];
+            })
+            // send message
+            .then(function () {
+                var stanza = ltx.parse(msg);
+                stanza.attrs.from = julia.jid.toString();
+                stanza.attrs.to = nickjid;
+                julia.send(stanza);
+
+            }).then(function (){
+                done();
+            }).catch (function (err) {
+                done(err);
+            });
+        });
+
+        /*
+         *
+         * Admin Sends Modified Ban List to Service
+         * <iq from='kinghenryv@shakespeare.lit/throne'
+         *     id='ban3'
+         *     to='southampton@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='outcast'
+         *           jid='lordscroop@shakespeare.lit'>
+         *       <reason>Treason</reason>
+         *     </item>
+         *     <item affiliation='outcast'
+         *           jid='sirthomasgrey@shakespeare.lit'>
+         *       <reason>Treason</reason>
+         *     </item>
+         *   </query>
+         * </iq>
+         *
+         * Service Informs Admin of Success
+         * <iq from='southampton@chat.shakespeare.lit'
+         *     id='ban3'
+         *     to='kinghenryv@shakespeare.lit/throne'
+         *     type='result'/>
+         */
+        it('9.2 Modifying the Ban List', function (done) {
+            done();
+        });
+
+        /*
+         *
+         * Admin Grants Membership
+         * <iq from='crone1@shakespeare.lit/desktop'
+         *     id='member1'
+         *     to='coven@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='member'
+         *           jid='hag66@shakespeare.lit'
+         *           nick='thirdwitch'/>
+         *   </query>
+         * </iq>
+         *
+         * Service Informs Admin of Success
+         * <iq from='coven@chat.shakespeare.lit'
+         *     id='member1'
+         *     to='crone1@shakespeare.lit/desktop'
+         *     type='result'/>
+         *
+         * Service Sends Notice of Membership to All Occupants
+         * <presence
+         *     from='coven@chat.shakespeare.lit/thirdwitch'
+         *     to='crone1@shakespeare.lit/desktop'>
+         *   <x xmlns='http://jabber.org/protocol/muc#user'>
+         *     <item affiliation='member'
+         *           jid='hag66@shakespeare.lit/pda'
+         *           role='participant'
+         *           nick='thirdwitch'/>
+         *   </x>
+         * </presence>
+         */
+
+        it('9.3 Granting Membership', function (done) {
+            done();
+        });
+
+        /*
+         *
+         * <iq from='crone1@shakespeare.lit/desktop'
+         *     id='member1'
+         *     to='coven@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='member'
+         *           jid='hag66@shakespeare.lit'
+         *           nick='thirdwitch'>
+         *       <reason>A worthy witch indeed!</reason>
+         *     </item>
+         *   </query>
+         * </iq>
+         */
+        it('9.3 Granting Membership', function (done) {
+            done();
+        });
+
+        /*
+         *
+         *
+         * Admin Revokes Membership
+         * <iq from='crone1@shakespeare.lit/desktop'
+         *     id='member2'
+         *     to='coven@chat.shakespeare.lit'
+         *     type='set'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='none'
+         *           jid='hag66@shakespeare.lit'/>
+         *   </query>
+         * </iq>
+         *
+         * Service Informs Moderator of Success
+         * <iq from='coven@chat.shakespeare.lit'
+         *     id='member2'
+         *     to='crone1@shakespeare.lit/desktop'
+         *     type='result'/>
+         *
+         * Service Notes Loss of Membership
+         * <presence
+         *     from='coven@chat.shakespeare.lit/thirdwitch'
+         *     to='crone1@shakespeare.lit/desktop'>
+         *   <x xmlns='http://jabber.org/protocol/muc#user'>
+         *     <item affiliation='none'
+         *           jid='hag66@shakespeare.lit/pda'
+         *           role='participant'/>
+         *   </x>
+         * </presence>
+         *
+         * Service Removes Non-Member
+         * <presence
+         *     from='coven@chat.shakespeare.lit/thirdwitch'
+         *     to='crone1@shakespeare.lit/desktop'>
+         *     type='unavailable'>
+         *   <x xmlns='http://jabber.org/protocol/muc#user'>
+         *     <item affiliation='none' role='none'>
+         *       <actor nick='TheBard'/>
+         *     </item>
+         *     <status code='321'/>
+         *   </x>
+         * </presence>
+         */
+        it('9.4 Revoking Membership', function (done) {
+            done();
+        });
+
+        /*
+         *
+         *
+         * Admin Requests Member List
+         * <iq from='crone1@shakespeare.lit/desktop'
+         *     id='member3'
+         *     to='coven@chat.shakespeare.lit'
+         *     type='get'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='member'/>
+         *   </query>
+         * </iq>
+         *
+         * Service Sends Member List to Admin
+         * <iq from='coven@chat.shakespeare.lit'
+         *     id='member3'
+         *     to='crone1@shakespeare.lit/desktop'
+         *     type='result'>
+         *   <query xmlns='http://jabber.org/protocol/muc#admin'>
+         *     <item affiliation='member'
+         *           jid='hag66@shakespeare.lit'
+         *           nick='thirdwitch'
+         *           role='participant'/>
+         *   </query>
+         * </iq>
+         *
+         */
+        it('9.5 Modifying the Member List', function (done) {
+            done();
+        });
+
+    });
+});
