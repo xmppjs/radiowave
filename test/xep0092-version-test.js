00a79720283b84c29617cdc2b575acc01503838f
Christoph Hartmann <chris@lollyrock.com>
2014-03-06 17:32:16 +0100

    fix all test

--- test/xep0092-version-test.js
+++ test/xep0092-version-test.js
@@ -3,33 +3,20 @@
 // assertion
 var assert = require('assert'),
     should = require('should'),
+    Promise = require('bluebird'),
+    ltx = require('ltx'),
     helper = require('./_helper/helper');
 
 // logging
 helper.configureLoglevel('silly');
 
-// xmpp client
-var ltx = require('ltx'),
-    Client = require('node-xmpp-client');
-
-// x rocket server
-var XRocket = require('../core/XRocket'),
-    Simple = require('../auth/Simple'),
-    C2SServer = require('xrocketd-cm').Net.C2SServer;
-
-// SASL Methods
-var Plain = require('node-xmpp-server/lib/authentication/plain');
-
 // Xep Components
-var ComponentRouter = require('../router/ComponentRouter'),
-    Xep0092 = require('../xep/Xep0092-version');
+var Xep0092 = require('../xep/Xep0092-version');
 
-// user
-var user = {
-    jid: 'admin@example.net',
-    password: 'password',
-    host: 'localhost'
-};
+function configureXEP(server) {
+    // register messaging component
+    server.cr.register(new Xep0092());
+}
 
 var NS_VERSION = 'jabber:iq:version',
     NAME = 'xRocket',
@@ -38,102 +25,62 @@ var NS_VERSION = 'jabber:iq:version',
 
 describe('Version', function () {
     describe('C2S', function () {
-        var xR = null;
+
+        var srv = null;
 
         before(function (done) {
-            // C2S Server 
-            var cs2 = new C2SServer({});
-            cs2.registerSaslMechanism(Plain);
-
-            // attach connection manager to xrocket
-            xR = new XRocket();
-            xR.addConnectionManager(cs2);
-
-            // register users
-            var simpleAuth = new Simple();
-            simpleAuth.addUser('admin', 'password');
-            xR.connectionRouter.authMethods.push(simpleAuth);
-
-            // register xep component
-            var cr = new ComponentRouter({
-                domain: 'example.net'
+            helper.startServer()
+            // configure muc module
+            .then(function (server) {
+                srv = server;
+                configureXEP(server);
+                done();
+            })
+                .
+            catch (function (err) {
+                done(err);
             });
-            cr.register(new Xep0092());
-
-            // chain XRocket to ComponentRouter
-            xR.chain(cr);
-
-            done();
         });
 
         after(function (done) {
-            xR.shutdown();
+            srv.xR.shutdown();
             done();
         });
 
-
-        it('should accept plain authentication', function (done) {
-            var cl = new Client({
-                jid: user.jid,
-                password: user.password,
-                preferred: 'PLAIN',
-                host: user.host
-            });
-
+        it('should return the version', function (done) {
+            // generate 
             var id = 'verion_1';
-
-            cl.on('stanza',
-                function (stanza) {
-                    if (stanza.is('iq')) {
-                        assert.equal(stanza.attrs.type, 'result');
-                        assert.equal(stanza.attrs.id, id);
-
-                        var query = stanza.getChild('query', NS_VERSION);
-                        assert.notEqual(query, null);
-
-                        assert.equal(query.getChild('name').getText(), NAME);
-                        assert.equal(query.getChild('version').getText(), VERSION);
-                        assert.equal(query.getChild('os').getText(), OS);
-
-                        done();
-                    } else {
-                        done('wrong stanza ' + stanza.root().toString());
-                    }
-                });
-
-            cl.on('online', function () {
-                // generate 
-                var version = new ltx.Element('iq', {
-                    to: 'example.net',
-                    from: cl.jid,
-                    type: 'get',
-                    id: id
-                }).c('query', {
-                    'xmlns': NS_VERSION
-                });
-                cl.send(version);
+            var stanza = new ltx.Element('iq', {
+                to: 'example.net',
+                from: helper.userRomeo.jid,
+                type: 'get',
+                id: id
+            }).c('query', {
+                'xmlns': NS_VERSION
             });
 
-            cl.on('error', function (e) {
-                console.error(e);
-                done(e);
-            });
+            helper.sendMessageWithRomeo(stanza.root()).then(function(stanza){
+                try {
 
-        });
+                    assert.equal(stanza.is('iq'),true, 'wrong stanza ' + stanza.root().toString());
 
-        it('should not accept plain authentication', function (done) {
-            var cl = new Client({
-                jid: user.jid,
-                password: 'secretsecret',
-                host: user.host
-            });
+                    assert.equal(stanza.attrs.type, 'result');
+                    assert.equal(stanza.attrs.id, id);
 
-            cl.on('online', function () {
-                done('user is not valid');
-            });
-            cl.on('error', function () {
-                // this should happen
-                done();
+                    var query = stanza.getChild('query', NS_VERSION);
+                    assert.notEqual(query, null);
+
+                    assert.equal(query.getChild('name').getText(), NAME);
+                    assert.equal(query.getChild('version').getText(), VERSION);
+                    assert.equal(query.getChild('os').getText(), OS);
+
+                    done();
+                   
+                } catch(err) {
+                    done(err);
+                }
+            }).catch(function(err){
+                done(err);
             });
 
         });
--- test/xep0199-ping-test.js
+++ test/xep0199-ping-test.js
@@ -3,127 +3,71 @@
 // assertion
 var assert = require('assert'),
     should = require('should'),
+    Promise = require('bluebird'),
+    ltx = require('ltx'),
     helper = require('./_helper/helper');
 
 // logging
 helper.configureLoglevel('silly');
 
-// xmpp client
-var ltx = require('ltx'),
-    Client = require('node-xmpp-client'),
-    Message = require('node-xmpp-core').Stanza.Message;
-
-// x rocket server
-var XRocket = require('../core/XRocket'),
-    Simple = require('../auth/Simple'),
-    C2SServer = require('xrocketd-cm').Net.C2SServer;
-
-// SASL Methods
-var Plain = require('node-xmpp-server/lib/authentication/plain');
-
 // Xep Components
-var ComponentRouter = require('../router/ComponentRouter'),
-    Xep0199 = require('../xep/Xep0199-ping');
+var Xep0199 = require('../xep/Xep0199-ping')
 
-
-// user
-var user = {
-    jid: 'admin@example.net',
-    password: 'password',
-    host: 'localhost'
-};
+function configureXEP(server) {
+    // register messaging component
+    server.cr.register(new Xep0199());
+}
 
 describe('Ping', function () {
     describe('C2S', function () {
-        var xR = null;
+
+        var srv = null;
 
         before(function (done) {
-            // C2S Server 
-            var cs2 = new C2SServer({});
-            cs2.registerSaslMechanism(Plain);
-
-            // attach connection manager to xrocket
-            xR = new XRocket();
-            xR.addConnectionManager(cs2);
-
-            // register users
-            var simpleAuth = new Simple();
-            simpleAuth.addUser('admin', 'password');
-            xR.connectionRouter.authMethods.push(simpleAuth);
-
-            // register xep component
-            var cr = new ComponentRouter({
-                domain: 'example.net'
+            helper.startServer()
+            // configure muc module
+            .then(function (server) {
+                srv = server;
+                configureXEP(server);
+                done();
+            })
+                .
+            catch (function (err) {
+                done(err);
             });
-            cr.register(new Xep0199());
-
-            // chain XRocket to ComponentRouter
-            xR.chain(cr);
-
-            done();
         });
 
         after(function (done) {
-            xR.shutdown();
+            srv.xR.shutdown();
             done();
         });
 
         it('should accept plain authentication and reply to ping', function (done) {
-            var cl = new Client({
-                jid: user.jid,
-                password: user.password,
-                preferred: 'PLAIN',
-                host: user.host
+            var id = 'ping-123';
+
+            var stanza = new ltx.Element('iq', {
+                to: 'example.net',
+                from: helper.userRomeo.jid,
+                type: 'get',
+                id: id
+            }).c('ping', {
+                'xmlns': 'urn:xmpp:ping'
             });
 
-            var id = 'r2d2';
+            helper.sendMessageWithRomeo(stanza.root()).then(function(stanza){
+                try {
 
-            cl.on('stanza',
-                function (stanza) {
-                    if (stanza.is('iq')) {
-                        assert.equal(stanza.attrs.type, 'result');
-                        assert.equal(stanza.attrs.id, id);
-                        done();
-                    } else {
-                        done('wrong stanza ' + stanza.root().toString());
-                    }
-                });
+                    assert.equal(stanza.is('iq'),true, 'wrong stanza ' + stanza.root().toString());
 
-            cl.on('online', function () {
-                // generate 
-                var ping = new ltx.Element('iq', {
-                    to: 'example.net',
-                    from: cl.jid,
-                    type: 'get',
-                    id: id
-                }).c('ping', {
-                    'xmlns': 'urn:xmpp:ping'
-                });
-                console.log(ping.root().toString());
-                cl.send(ping);
-            });
-
-            cl.on('error', function (e) {
-                console.log(e);
-                done(e);
-            });
-
-        });
-
-        it('should not accept plain authentication', function (done) {
-            var cl = new Client({
-                jid: user.jid,
-                password: 'secretsecret',
-                host: user.host
-            });
-
-            cl.on('online', function () {
-                done('user is not valid');
-            });
-
-            cl.on('error', function () {
-                // this should happen
-                done();
+                    assert.equal(stanza.attrs.type, 'result');
+                    assert.equal(stanza.attrs.id, id);
+                    done();
+                   
+                } catch(err) {
+                    done(err);
+                }
+            }).catch(function(err){
+                done(err);
             });
 
         });
