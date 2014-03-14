c8ff6275379d6300e07c6b2238ecf9a15823cc15
Christoph Hartmann <chris@lollyrock.com>
2014-03-14 14:26:29 +0100

    user jids must be unique.

--- storage/models/User.js
+++ storage/models/User.js
@@ -11,6 +11,7 @@ module.exports = function (sequelize, DataTypes) {
         },
         jid: {
             type: DataTypes.STRING,
+            unique: true,
             validate: {}
         },
         uuid: {
